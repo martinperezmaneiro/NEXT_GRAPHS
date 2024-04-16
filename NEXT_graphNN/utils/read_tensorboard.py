bf77724f9b54ab0cedf560e9a20c6a448be18b35
Martin <martinperezmaneiro@gmail.com>
2024-04-16 12:24:31 +0200

    Add read tensorboard utils

---
+++ NEXT_graphNN/utils/read_tensorboard.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from tensorboard.backend.event_processing import event_accumulator
+
+def load_tensorboard_df(path):
+    ea = event_accumulator.EventAccumulator(path)
+    ea.Reload()
+    values = ea.Tags()['scalars']
+    
+    if not values:
+        #p = path.split('/')
+        #warnings.warn("The file {}, in the folder {}, does not contain any data".format(p[-1], p[-3]))
+        return pd.DataFrame()
+    else:
+        #take one of them that share wall time and step to be the base
+        df = pd.DataFrame(ea.Scalars(values[0]))[['wall_time', 'step']]
+
+        #iterate over the values to get the data
+        for val in values:
+            colname = val.replace('/', '_')
+            df = df.assign(**{colname : pd.DataFrame(ea.Scalars(val)).value})
+        return df
